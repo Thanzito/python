ec4b82bf754c539532f3fa7ff11be95e7bfe4592
Thanzito <163420206+Thanzito@users.noreply.github.com>
2025-02-17 10:11:25 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -13,5 +13,15 @@ print("2 - Listar restaurante")
 print("3 - Ativar restaurante")
 print("4 - Sair")
 
-opcao_escolhida = input('Escolha uma opção: ')
+opcao_escolhida = int(input('Escolha uma opção: '))
 print(f"Você escolheu a opção: {opcao_escolhida}")
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurante')
+elif opcao_escolhida == 3:
+    print('Ativar restaurante')
+else:
+    print('Encerrando programa')
+  
