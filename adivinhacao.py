4618fd95252bf2af6c43f07b084e826f3947a0c7
Thanzito <163420206+Thanzito@users.noreply.github.com>
2025-02-27 12:06:32 -0300

    Add files via upload

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,6 +4,7 @@ print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
+pontos = 1000
 numero_secreto = random.randrange(1,101)
 total_de_tentativas = 0
 
@@ -20,9 +21,7 @@ else:
 
 for rodada in range(1,total_de_tentativas+1):
     print("tentativa {} de {}".format(rodada,total_de_tentativas))
-    chute_str = input("digite seu número: ")
-    print("Você digitou",chute_str)
-    chute = int(chute_str)
+    chute = int(input("digite seu número: "))
 
     if(chute < 1 or chute > 100):
        print("Você deve digitar um número entre 1 e 100!")
@@ -32,12 +31,13 @@ for rodada in range(1,total_de_tentativas+1):
     maior = chute > numero_secreto
     menor = chute < numero_secreto
     if(acertou):
-        print("Você acertou!")
+        print("Você acertou! e fez {} pontos! Parabéns!".format(pontos))
         break
     else:
         if(maior):
           print("Você errou! 0 seu chute foi maior que o número secreto.")
         elif(menor):
           print("Você errou! 0 seu chute foi menor que o número secreto.")
-
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 print("Fim do jogo!")
